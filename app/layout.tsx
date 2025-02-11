96ab5225f30c3f8718eda48062d8bc273a0530d0
Arianit Sylafeta <ari.sylafeta@gmail.com>
2025-02-11 00:01:13 +0000

    fix clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
       <MicrosoftClarity />
-      <head />
+      </head>
       <body className="min-h-screen bg-background font-sans antialiased">
         <ThemeProvider
           attribute="class"
